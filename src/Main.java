1ee04f46429b530e69c62fbb6e9d8bd530ccb850
Sharipovaa <alfya.yanbekova28@gmail.com>
2022-02-16 17:39:59 +0500

    added fixes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int cost = 8000;
+        int cost = 10000;
         int miles = service.calculate(cost);
         System.out.println(miles);
     }
